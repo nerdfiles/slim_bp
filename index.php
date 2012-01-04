458616bacf9d863fe56763f49d039f24364ef9fb
Aaron Alexander <nerdfiles@gmail.com>
2012-01-03 21:53:42 -0600

    clenaup

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+// log php errors
+@ini_set('log_errors','On'); // enable or disable php error logging (use 'On' or 'Off')
+@ini_set('display_errors','On'); // enable or disable public display of errors (use 'On' or 'Off')
+@ini_set('error_log','log/errors.log'); // path to server-writable log file
+
 require 'Slim/Slim.php';
 
 $app = new Slim();
